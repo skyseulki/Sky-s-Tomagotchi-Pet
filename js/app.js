6ef313faffd916c36911c2d3864c0017fce44af2
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-04-01 13:00:18 -0700

    created html, css, js, & added class to js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+class Tomagotchi {
+    constructor() {
+        this.name = name;
+        this.age = age;
+        this.hunger = 10;
+        this.sleepiness = 10;
+        this.boredom = 10;
+
+    }
+};
